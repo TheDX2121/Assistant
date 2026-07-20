db3ad22d24c6f9835406dfc3b6e0fe633168ff95
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-07-20 09:52:20 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -4,82 +4,62 @@ from database.mongo import db
 users = db.users
 
 
+
 async def add_user(
-    user_id: int,
-    first_name: str,
-    username: str = None
+    user_id,
+    first_name,
+    username
 ):
 
-    data = {
-        "user_id": user_id,
-        "first_name": first_name,
-        "username": username,
-
-        "profile_link":
-            f"tg://user?id={user_id}",
-
-        "trusted": False,
-
-        "vip": False,
-        "vip_expiry": None,
-
-        "blacklisted": False,
-
-        "total_appointments": 0,
-        "total_tickets": 0,
-        "total_emergencies": 0,
-
-        "created_at": None,
-        "updated_at": None
-    }
-
-
     await users.update_one(
+
         {
-            "user_id": user_id
+            "user_id":
+                user_id
         },
 
         {
-            "$setOnInsert": data
+            "$set": {
+
+                "user_id":
+                    user_id,
+
+                "first_name":
+                    first_name,
+
+                "username":
+                    username
+            }
         },
 
         upsert=True
     )
 
 
+
 async def get_user(
-    user_id: int
+    user_id
 ):
 
     return await users.find_one(
         {
-            "user_id": user_id
+            "user_id":
+                user_id
         }
     )
 
-
-async def update_user(
-    user_id: int,
-    data: dict
-):
-
-    await users.update_one(
-        {
-            "user_id": user_id
-        },
-
-        {
-            "$set": data
-        }
-    )
 
 
 async def get_all_users():
 
     result = []
 
+
     async for user in users.find():
 
-        result.append(user)
+        result.append(
+            user
+        )
+
 
     return result
