aee662c4f9a5e7f60295770ae384eac9bb654238
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-07-20 10:18:20 +0530

    Update version.py

--- version.py
+++ version.py
@@ -1,31 +1,15 @@
-"""
-Manager Bot Version Information
-"""
+VERSION = "1.0"
 
-BOT_NAME = "Manager Bot"
-
-VERSION = "1.0.0"
-
-BUILD = "2026.07.20"
-
-AUTHOR = "𝚂𝚊𝚖𝚞𝚎𝚕~"
-
-DESCRIPTION = (
-    "Personal Assistant Telegram Bot "
-    "with Appointment, Emergency, Ticket, "
-    "VIP and Management Systems."
-)
-
-
-def get_version():
-    return VERSION
 
 
 def get_info():
+
     return {
-        "name": BOT_NAME,
-        "version": VERSION,
-        "build": BUILD,
-        "author": AUTHOR,
-        "description": DESCRIPTION,
+
+        "version":
+            VERSION,
+
+        "name":
+            "Personal Assistant Bot"
+
     }
