8e9a2691d1529e33cec548177beb1b586cd8665a
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-07-20 09:53:23 +0530

    Update tickets.py

--- database/tickets.py
+++ database/tickets.py
@@ -4,76 +4,48 @@ from database.mongo import db
 tickets = db.tickets
 
 
+
 async def create_ticket(
-    data: dict
+    data
 ):
 
-    await tickets.insert_one(data)
-
-
-async def get_ticket(
-    ticket_id: str
-):
-
-    return await tickets.find_one(
-        {
-            "ticket_id": ticket_id
-        }
+    await tickets.insert_one(
+        data
     )
 
-
-async def update_ticket(
-    ticket_id: str,
-    data: dict
-):
-
-    await tickets.update_one(
-        {
-            "ticket_id": ticket_id
-        },
-
-        {
-            "$set": data
-        }
-    )
 
 
 async def add_message(
-    ticket_id: str,
-    message: dict
+    ticket_id,
+    message
 ):
 
     await tickets.update_one(
+
         {
-            "ticket_id": ticket_id
+            "ticket_id":
+                ticket_id
         },
 
         {
-            "$push": {
-                "messages": message
-            },
-
-            "$set": {
-                "last_message":
+            "$push":
+            {
+                "messages":
                     message
             }
         }
     )
 
 
-async def get_user_tickets(
-    user_id: int
+
+async def get_ticket(
+    ticket_id
 ):
 
-    result = []
+    return await tickets.find_one(
 
-    async for ticket in tickets.find(
         {
-            "user_id": user_id
+            "ticket_id":
+                ticket_id
         }
-    ):
-
-        result.append(ticket)
-
-
-    return result
+    )
